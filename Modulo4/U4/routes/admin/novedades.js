9c0c09b026ef63de45a6e3f121a930372f8249df
Juan Pablo Bracken <juanpablobracken@gmail.com>
2021-11-02 11:18:38 -0300

    u4

--- Modulo4/U4/routes/admin/novedades.js
+++ Modulo4/U4/routes/admin/novedades.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('/admin/novedades', {
+    res.render('admin/novedades', {
         layout: 'admin/layout',
         usuario: req.session.nombre
     });
