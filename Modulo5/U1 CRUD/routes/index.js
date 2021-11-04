e904dc97c2f883639934a825634b5d1fc92498ca
Juan Pablo Bracken <juanpablobracken@gmail.com>
2021-11-04 13:43:10 -0300

    CRUD

---
+++ Modulo5/U1 CRUD/routes/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer')
+var novedadesModel = require('./../models/novedadesModel');
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+
+  res.render('index',{
+    novedades
+  });
+});
+
+router.post('/', async (req, res, next) => {
+
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var celular = req.body.celular;
+  var email = req.body.email;
+  var mensaje = req.body.mensaje;
+
+  console.log(req.body);
+
+  var obj = {
+    to: 'juanpablobracken@gmail.com',
+    subject: 'Contacto desde la Web',
+    html: nombre + " " + apellido + " se contacto a traves del sitio y quiere mas info a este correo: " + email + ". <br> Además, hizo el siguiente comentario: " + mensaje + ".<br> Su tel es " + celular
+  }
+
+  var transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  })
+
+  var info = await transporter.sendMail(obj);
+  
+  res.render('index', {
+    mensaje: 'Mensaje enviado correctamente',
+  });
+
+});
+
+
+
+module.exports = router;
